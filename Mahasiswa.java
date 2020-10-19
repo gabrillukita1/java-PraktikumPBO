80bdd8eb4de817bb28f9e39351b20a40960f5f32
gabriel <gabrillukita@gmail.com>
2020-10-19 14:13:39 +0700

    tugas 1

---
+++ Mahasiswa.java
@@ -0,0 +1,68 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Mahasiswa {
+    private String nama;
+    private String nim;
+    private String alamat;
+    private int uts;
+    private int uas;
+    
+    public Mahasiswa(String nama, String nim, String alamat, int uts, int uas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.alamat = alamat;
+        this.uts = uts;
+        this.uas = uas;
+    }
+    
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    
+    public void setNim(String nim){
+        this.nim = nim;
+    }
+    
+    public void setAlamat(String alamat){
+        this.alamat = alamat;
+    }
+    
+    public void setUts(int uts){
+        this.uts = uts;
+    }
+    
+    public void setUas(int uas){
+        this.uas = uas;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    
+    public String getNim(){
+        return nim;
+    }
+    
+    public String getAlamat(){
+        return alamat;
+    }
+    
+    public int getUts(){
+        return uts;
+    }
+    
+    public int getUas(){
+        return uas;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,73 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nMasukan Nama    : ");
+        String nama = scanner.nextLine();
+        System.out.print("Masukan NIM     : ");
+        String nim = scanner.nextLine();
+        System.out.print("Masukan Alamat  : ");
+        String alamat = scanner.nextLine();
+        System.out.print("Masukan UTS     : ");
+        int uts = scanner.nextInt();
+        System.out.print("Masukan UAS     : ");
+        int uas = scanner.nextInt();
+        
+        Mahasiswa mahasiswa1 = new Mahasiswa(nama, nim, alamat, uts, uas);
+        
+        
+        int menu = 0;
+        do{
+            System.out.println("\n\nMenu");
+            System.out.println("1. Lihat Data");
+            System.out.println("2. Edit Data");
+            System.out.println("3. Keluar");
+            System.out.print("Pilih : ");
+            menu = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch(menu){
+                case 1 : 
+                    System.out.println("\n\nNama    : " + mahasiswa1.getNama());
+                    System.out.println("NIM     : " + mahasiswa1.getNim());
+                    System.out.println("Alamat  : " + mahasiswa1.getAlamat());
+                    System.out.println("Jurusan : " + mahasiswa1.getUts());
+                    System.out.println("Nilai   : " + mahasiswa1.getUas());
+                    break;
+                case 2 : 
+                    System.out.println("\n\nEdit Data");
+                    
+                    System.out.print("\nMasukan Nama    : ");
+                    nama = scanner.nextLine();
+                    mahasiswa1.setNama(nama);
+                    System.out.print("Masukan NIM     : ");
+                    nim = scanner.nextLine();
+                    mahasiswa1.setNim(nim);
+                    System.out.print("Masukan Alamat  : ");
+                    alamat = scanner.nextLine();
+                    mahasiswa1.setAlamat(alamat);
+                    System.out.print("Masukan UTS     : ");
+                    uts = scanner.nextInt();
+                    mahasiswa1.setUts(uts);
+                    System.out.print("Masukan UAS     : ");
+                    uas = scanner.nextInt();
+                    mahasiswa1.setUas(uas);
+                    break;
+                case 3 :
+                    break;
+            }
+        }while(menu != 3);
+    }
+}
